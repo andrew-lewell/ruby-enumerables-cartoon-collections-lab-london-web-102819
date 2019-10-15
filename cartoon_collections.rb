913977fed89c0792e06dd780f2df1d2d49394fa7
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-15 14:05:26 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,5 +13,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  
+  if array.include("cheddar") = true then 
+    return "cheddar"
+  elsif array.include
 end
