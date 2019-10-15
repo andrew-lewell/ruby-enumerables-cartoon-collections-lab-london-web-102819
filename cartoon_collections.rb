ca36c16f4d3d7ed53a6a46c4a80d4d2d7c7b514f
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-15 13:44:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(array)
   array.map! { |element| element.capitalize + "!" }
 end
 
-def long_planeteer_calls
-  # Your code here
+def long_planeteer_calls(array)
+  
 end
 
 def find_the_cheese# code an argument here
